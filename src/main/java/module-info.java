52d64b2a37a88c9c2df7e89babdb89e2608b99d6
Marc Magon <ged_marc@hotmail.com>
2025-02-28 21:57:36 +0200

    Seperate Web from Vertx

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -1,9 +1,12 @@
 import com.guicedee.telemetry.implementations.GuiceTelemetryModuleInclusions;
+import com.guicedee.telemetry.spi.GuiceTelemetryRegistration;
 
 module com.guicedee.telemetry {
     requires transitive com.guicedee.vertx;
     requires io.vertx.tracing.opentelemetry;
 
     provides com.guicedee.guicedinjection.interfaces.IGuiceScanModuleInclusions with GuiceTelemetryModuleInclusions;
+
+    uses GuiceTelemetryRegistration;
 
 }
